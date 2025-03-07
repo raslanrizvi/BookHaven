5cdd3937d08cd717597c9fac3b0487784f04b553
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-03-07 21:24:29 +0530

    Completed Supplier Model

---
+++ BookHaven/Models/SupplierModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHaven.Models
+{
+    public class SupplierModel
+    {
+        public int id;
+        public string name;
+        public int typeId;
+        public string typeName;
+        public string mobile;
+        public string email;
+        public string address;
+        public int curntStock;
+
+    }
+}
