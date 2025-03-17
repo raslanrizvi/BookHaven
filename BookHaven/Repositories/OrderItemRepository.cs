878789a5543ddca0fef0026c6a0045f7543af24b
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-03-17 22:17:58 +0530

    Completed Update Order

--- BookHaven/Repositories/OrderItemRepository.cs
+++ BookHaven/Repositories/OrderItemRepository.cs
@@ -13,6 +13,50 @@ namespace BookHaven.Repositories
     {
         private string connectionString = "Data Source=RASLANS_TUF;Initial Catalog=bookHaven;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
 
+        //To load Orders items to Bill Data Grid
+        public List<OrderItemsModel> GetOrderItems(int ordId)
+        {
+            var orderItems = new List<OrderItemsModel>();
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    string sql = "SELECT o.*, b.Title FROM OrderItems o INNER JOIN Books b ON o.BookId = b.ID WHERE OrdId=@ordId ORDER BY ID DESC";
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@ordId", ordId);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                OrderItemsModel ordItm = new OrderItemsModel();
+                                ordItm.id = reader.GetInt32(0);
+                                ordItm.ordId = reader.GetInt32(1);
+                                ordItm.bookId = reader.GetInt32(2);
+                                ordItm.qty = reader.GetInt32(3);
+                                ordItm.price = reader.GetDecimal(4);
+                                ordItm.tot = reader.GetDecimal(5);
+                                ordItm.title = reader.GetString(6);
+
+
+                                orderItems.Add(ordItm);
+                            }
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Read Data To Data Grid Bill Items Order Error : " + ex.ToString());
+            }
+            return orderItems;
+        }
+
         //To Create New Order Item Record
         public void CreateNewOrderItem(OrderItemsModel ordItm)
         {
@@ -54,6 +98,31 @@ namespace BookHaven.Repositories
             catch (Exception ex)
             {
                 MessageBox.Show("Add Order Items error" + ex.ToString());
+            }
+        }
+
+        //Delete an Existing Order Item
+        public void DeleteOrderItem(int id)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    string sql = "DELETE FROM OrderItems WHERE ID=@id";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception Delete Order Item : " + ex.ToString());
             }
         }
     }
